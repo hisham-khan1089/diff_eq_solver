811e40dc81c017b361daf5f33fe94f70d12ae9ab
Hisham Khan <hikha9089@gmail.com>
2025-09-14 23:00:26 -0400

    Added custom x and y range input to direction_field and diff_eq_solver

--- diff_eq_solver.py
+++ diff_eq_solver.py
@@ -3,11 +3,11 @@ import numpy as np
 import math
 
 def diff_eq_function(x: float, y: float) -> float:
-    return math.sin(x**y)
+    return 100
 
 diff_eq_function_np = np.vectorize(diff_eq_function)
 
-def direction_field(range: list[float, float], n: int | None = None) -> list[list]:
+def direction_field(rangex: list[float, float], rangey: list[float, float], n: int | None = None) -> list[list]:
     
     """Generates the meshgrids (matrices) used to create a direction field of 
     a given first-order differential equation using plt.quiver.
@@ -18,8 +18,8 @@ def direction_field(range: list[float, float], n: int | None = None) -> list[lis
 
     if n == None:
         n = 20
-    x = np.linspace(range[0], range[1], n)
-    y = np.linspace(range[0], range[1], n)
+    x = np.linspace(rangex[0], rangex[1], n)
+    y = np.linspace(rangey[0], rangey[1], n)
 
     X, Y = np.meshgrid(x, y) 
     
@@ -54,14 +54,14 @@ def euler(initial_cond: list[float], final_x: float, n: float | None = None) -> 
      
     return y
     
-def diff_eq_solver(initial_cond: list[float], range: list[float, float]) -> None:
+def diff_eq_solver(initial_cond: list[float], rangex: list[float, float], rangey: list[float, float]) -> None:
     
     """Create matplotlib plots that shows a numerical solution to first-order differential equation
     for a given range, alongside a direction field.
     
     range: list containing lower and upper bound for both x and y axis"""
 
-    x_values = np.linspace(range[0], range[1], 30)
+    x_values = np.linspace(rangex[0], rangex[1], 30)
 
     y_values = []
 
@@ -71,7 +71,7 @@ def diff_eq_solver(initial_cond: list[float], range: list[float, float]) -> None
     y_values = np.array(y_values)
 
     plt.plot(x_values, y_values)
-    ylim_fixer = (range[1]-range[0])*0.05
+    ylim_fixer = (rangey[1]-rangey[0])*0.05
 
     # Previously the ylim was determined by the graph of the solution to the 
     # differential equation, meaning it could blow up the upper bound of the
@@ -81,12 +81,12 @@ def diff_eq_solver(initial_cond: list[float], range: list[float, float]) -> None
     # is slightly larger than the upper bound of range and slightly lower than the 
     # lower bound of range (so that the direction field arrows are not cut off)
 
-    plt.ylim(range[0]-ylim_fixer, range[1]+ylim_fixer) 
+    plt.ylim(rangey[0]-ylim_fixer, rangey[1]+ylim_fixer)
 
-    dir_field = direction_field([0, 4])
+    dir_field = direction_field(rangex, rangey)
     plt.quiver(dir_field[0], dir_field[1], dir_field[2], dir_field[3])
     plt.show()
 
-diff_eq_solver([0, 2], [0, 4])
+diff_eq_solver([0, 53], [0, 4], [0, 1000])
 
 
